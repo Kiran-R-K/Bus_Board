19d266c6ea46dd388a4a8107d4f068bffb827802
Kiran Randhawa Kukar <Kiran.Randhawa-Kukar@softwire.com>
2022-09-09 16:19:09 +0100

    busboard can take a bus ID and return the 5 buses predicted to arrive next and their info

--- bus_board.rb
+++ bus_board.rb
@@ -1,16 +1,36 @@
+require 'open-uri'
+require 'json'
 
 class BusBoard
-  attr_reader :bus_stop
+  attr_reader :bus_stop_id
 
   def start
     puts 'Intput the bus stop code you want to check'
-    @bus_stop = gets.chomp
-    request_api
+    @bus_stop_id = gets.chomp
+    request_api_data
   end
 
-  def request_api
-
+  def request_api_data
+    url = "https://api.tfl.gov.uk/StopPoint/#{@bus_stop_id}/Arrivals"
+    url_data = URI.open(url).read
+    json_data =  JSON.parse(url_data)
+    find_soonest_busses(json_data)
   end
+
+  def find_soonest_busses(data)
+    sorted_busses = data.sort_by { |bus| bus["timeToStation"] }
+    five_soonest_busses = sorted_busses[0..4]
+    bus_arrivals(five_soonest_busses)
+  end
+
+  def bus_arrivals(busses_arriving)
+    for bus in busses_arriving do
+      arrival_time = bus["timeToStation"]/60
+      puts "Bus: #{bus["lineId"]} | Destination: #{bus["destinationName"]} | Arriving in: #{arrival_time}mins"
+    end
+  end
+
+
 end
 
 
