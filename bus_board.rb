04018a4a441c61df17c8dc34f1718ea7750768fd
Kiran Randhawa Kukar <Kiran.Randhawa-Kukar@softwire.com>
2022-09-09 16:49:20 +0100

    seperated out api classes in advance of postcode intput implementation

--- bus_board.rb
+++ bus_board.rb
@@ -1,33 +1,15 @@
+require './postcode_api.rb'
+require './tfl_bus_arrival_api.rb'
+
 require 'open-uri'
 require 'json'
 
 class BusBoard
   attr_reader :bus_stop_id
 
-  def start
-    puts 'Intput the bus stop code you want to check'
-    @bus_stop_id = gets.chomp
-    request_api_data
-  end
-
-  def request_api_data
-    url = "https://api.tfl.gov.uk/StopPoint/#{@bus_stop_id}/Arrivals"
-    url_data = URI.open(url).read
-    json_data =  JSON.parse(url_data)
-    find_soonest_busses(json_data)
-  end
-
-  def find_soonest_busses(data)
-    sorted_busses = data.sort_by { |bus| bus["timeToStation"] }
-    five_soonest_busses = sorted_busses[0..4]
-    bus_arrivals(five_soonest_busses)
-  end
-
-  def bus_arrivals(busses_arriving)
-    for bus in busses_arriving do
-      arrival_time = bus["timeToStation"]/60
-      puts "Bus: #{bus["lineId"]} | Destination: #{bus["destinationName"]} | Arriving in: #{arrival_time}mins"
-    end
+  def initialize
+    bus_arrival_api = TflBusArrivalAPI.new
+    bus_arrival_api.request_data('490008660N')
   end
 
 
@@ -35,6 +17,4 @@ end
 
 
 
-bus_board = BusBoard.new
-
-bus_board.start
+BusBoard.new
---
+++ tfl_bus_arrival_api.rb
@@ -0,0 +1,27 @@
+require 'open-uri'
+require 'json'
+
+class TflBusArrivalAPI
+
+
+  def request_data(bus_stop_id)
+    url = "https://api.tfl.gov.uk/StopPoint/#{bus_stop_id}/Arrivals"
+    url_data = URI.open(url).read
+    json_data =  JSON.parse(url_data)
+    find_soonest_busses(json_data)
+  end
+
+  def find_soonest_busses(data)
+    sorted_busses = data.sort_by { |bus| bus["timeToStation"] }
+    five_soonest_busses = sorted_busses[0..4]
+    display_bus_arrivals(five_soonest_busses)
+  end
+
+  def display_bus_arrivals(busses_arriving)
+    for bus in busses_arriving do
+      arrival_time = bus["timeToStation"]/60
+      puts "Bus: #{bus["lineId"]} | Destination: #{bus["destinationName"]} | Arriving in: #{arrival_time}mins"
+    end
+  end
+
+end
